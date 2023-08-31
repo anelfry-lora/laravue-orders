a88d189176cbe776bff16013704ff0a2b00c64d4
Anelfry Lora <anelfrylora@gmail.com>
2023-08-31 11:14:29 -0400

    fix error in component layouts

--- vue/src/views/layouts/Page.vue
+++ vue/src/views/layouts/Page.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { defineProps } from 'vue';
-import Header from '../../components/Header.vue';
-import MainContent from '../../components/MainContent.vue';
+import Header from '../../components/layouts/Header.vue';
+import MainContent from '../../components/layouts/MainContent.vue';
 
 const props = defineProps({
     title: String,
